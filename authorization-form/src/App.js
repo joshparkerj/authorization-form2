10286e8af9b4e7f7ab4dd5cc6f8ea22e6079a05d
joshparkerj <superfakek@gmail.com>
2018-07-25 17:10:47 -0600

    Add (simplistic) authorization logic

--- authorization-form/src/App.js
+++ authorization-form/src/App.js
@@ -22,17 +22,30 @@ class Contact extends Component {
 	}
 	
 	render() {
+		let authorizationHeading = this.state.authorized ? 'Contact' : 'Enter the Password';
+		
+		let login = (
+			<form action="#" onSubmit={this.authorize}>
+				<input type="password" placeholder="password" />
+				<input type="submit" />
+			</form>
+		);
+		
+		const contactInfo = (
+			<ul>
+				<li>
+					client@example.com
+				</li>
+				<li>
+					555.555.5555
+				</li>
+			</ul>
+		);
+		
 		return (
 			<div id="authorization">
-				<h1>Contact</h1>
-				<ul>
-					<li>
-						client@example.com
-					</li>
-					<li>
-						555.555.5555
-					</li>
-				</ul>
+				<h1>{authorizationHeading}</h1>
+				{this.state.authorized ? contactInfo : login}
 			</div>
 		);
 	}		
